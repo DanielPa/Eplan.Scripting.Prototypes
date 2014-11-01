28f18b8fd36f759730de28161dfd7833cad5811f
DanielPa <papp.technik@gmail.com>
2014-11-01 23:37:29 +0100

    License

--- MacroNavi/Templates/MacroNaviForm.cs
+++ MacroNavi/Templates/MacroNaviForm.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Forms;
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ 
+using System.Windows.Forms;
 using Eplan.EplApi.Scripting;
 using System.IO;
 using System.Xml;
